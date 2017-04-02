2824f2e52fd276381834c5d16a4244669aa44b91
Mike Buckley <mikebuckley.canada@gmail.com>
2017-04-02 12:37:03 -0700

    Added simple fixture

--- functions/users-get/index.ts
+++ functions/users-get/index.ts
@@ -1,9 +1,15 @@
-import beavers from "./beavers.fixture";
+import beavers from "./fixtures/beavers.fixture";
 
 export function handler(event: any, context: any, callback: any) {
   console.log("event", event);
   console.log("context 2", context);
   console.log("beavers", beavers);
 
-  callback(null, { statusCode: 200, headers: {}, body: "success beavers!" });
+  callback(null, {
+    statusCode: 200,
+    headers: {
+      "Content-Type" : "text/json",
+    },
+    body: JSON.stringify(beavers),
+  });
 }
