3b97c9257aae051d50257efb0974632f68f8f800
Mike Buckley <mikebuckley.canada@gmail.com>
2017-04-02 11:58:02 -0700

    Added compileOnSave so run local works

---
+++ functions/users-get/beavers.fixture.ts
@@ -0,0 +1,26 @@
+export default [
+  {
+    name: "Mike Buckley",
+    nickname: "buckles",
+    skills: [{
+      name: "",
+      description: "",
+    }],
+  },
+  {
+    name: "Kit Brown-Watts",
+    nickname: "kittens",
+    skills: [{
+      name: "",
+      description: "",
+    }],
+  },
+  {
+    name: "Chris Saunders",
+    nickname: "kernel",
+    skills: [{
+      name: "",
+      description: "",
+    }],
+  },
+];
--- functions/users-get/index.ts
+++ functions/users-get/index.ts
@@ -1,34 +1,6 @@
 export function handler(event: any, context: any, callback: any) {
   console.log("event", event);
-  console.log("context2", context);
-  // ignore context
-
-  // const beavers = [
-  //   {
-  //     name: "Mike Buckley",
-  //     nickname: "buckles",
-  //     skills: [{
-  //       name: "",
-  //       description: "",
-  //     }],
-  //   },
-  //   {
-  //     name: "Kit Brown-Watts",
-  //     nickname: "kittens",
-  //     skills: [{
-  //       name: "",
-  //       description: "",
-  //     }],
-  //   },
-  //   {
-  //     name: "Chris Saunders",
-  //     nickname: "kernel",
-  //     skills: [{
-  //       name: "",
-  //       description: "",
-  //     }],
-  //   },
-  // ];
+  console.log("context", context);
 
   callback(null, { statusCode: 200, headers: {}, body: "success beavers!" });
 }
